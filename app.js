dc9a41e2090e1ef99ce92de6781f85fa394c6e20
Altynbekow <152465429+Altynbekow@users.noreply.github.com>
2023-12-11 17:25:16 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function countchar(Salamat,a){
+    for(let i=0;i<Salamat.length; i++){
+        if(Salamat[i]===a){
+            console.log("Salamat")
+        }
+    }
+}
+countchar("Salamat ","a")
